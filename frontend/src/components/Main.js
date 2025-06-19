8b1ca475e08115ca03a93b4844371f259b82ebf7
Prabhudda G B Goudar <prabhuddagb.goudar@wipro.com>
2025-06-20 02:53:05 +0530

    updated main file

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -35,3 +35,20 @@ function Main() {
 }
 
 export default Main;
+
+
+
+
+
+// import React from 'react';
+
+// function Main() {
+//   return (
+//     <main className="main">
+//       <h1>Developer Portal</h1>
+//       <p style={{fontSize:"19px"}}>Welcome to the Developer Portal</p>
+//     </main>
+//   );
+// }
+
+// export default Main;
