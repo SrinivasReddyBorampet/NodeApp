ed4bbc2821a70138c6389fd4f5304d6b17d2a0b9
Prabhudda G B Goudar <prabhuddagb.goudar@wipro.com>
2025-06-20 01:28:53 +0530

    updated main file

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 function Main() {
+
+  const password = "123456";
+  const test = () => {
+    console.log("test")
+  }
+
   return (
     <main className="main">
-      <h1>Developer Portal</h1>
-      <p style={{fontSize:"19px"}}>Welcome to the Developer Portal</p>
+      <h1  id='test'>Developer Portal</h1>
+      <p style={{fontSize:"19px"}} id='test'>Welcome to the Developer Portal</p>
     </main>
   );
 }
